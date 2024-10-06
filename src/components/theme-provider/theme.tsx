94fb089396280b2b331de35ac7fec3ba98561a02
Carrie Forde <carrie@carrieforde.com>
2024-10-06 11:04:56 -0700

    add theme styles

---
+++ src/components/theme-provider/theme.tsx
@@ -0,0 +1,45 @@
+import { IconCircleCheck } from '@/components/icons/icon-circle-check';
+import { IconCircleExclamation } from '@/components/icons/icon-circle-exclamation';
+import { IconCircleInfo } from '@/components/icons/icon-circle-info';
+import { IconCircleNotch } from '@/components/icons/icon-circle-notch';
+
+export type Theme = {
+  components: {
+    alert: {
+      defaultDurationInMs: number;
+      // icons: AlertIconConfig;
+    };
+    banner: {
+      // icons: BannerIconConfig;
+    };
+    spinner: {
+      icon: React.ReactNode;
+    };
+  };
+};
+
+export const theme: Theme = {
+  components: {
+    alert: {
+      defaultDurationInMs: 5000,
+      // icons: {
+      //   info: <IconCircleInfo />,
+      //   error: <IconCircleExclamation />,
+      //   warning: <IconCircleExclamation />,
+      //   success: <IconCircleCheck />,
+      // },
+    },
+    banner: {
+      icons: {
+        default: null,
+        info: <IconCircleInfo />,
+        error: <IconCircleExclamation />,
+        warning: <IconCircleExclamation />,
+        success: <IconCircleCheck />,
+      },
+    },
+    spinner: {
+      icon: <IconCircleNotch />,
+    },
+  },
+};
