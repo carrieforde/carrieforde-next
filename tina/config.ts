4f880477abeb2178d28ec20c39c8481c4c0c3a75
Carrie Forde <carrie@carrieforde.com>
2025-04-27 16:17:22 -0700

    set up Tina schema

--- tina/config.ts
+++ tina/config.ts
@@ -7,6 +7,69 @@ const branch =
   process.env.HEAD ||
   "main";
 
+const templates: Template[] = [
+  {
+    name: "Banner",
+    label: "Banner",
+    fields: [
+      {
+        name: "children",
+        label: "Content",
+        type: "rich-text",
+      },
+      {
+        name: "variant",
+        label: "Variant",
+        type: "string",
+        options: ["info", "warning", "error", "success"],
+      },
+    ],
+  },
+  {
+    name: "Image",
+    label: "Custom Image",
+    fields: [
+      {
+        name: "src",
+        label: "Image",
+        type: "image",
+      },
+      {
+        name: "alt",
+        label: "Alt Text",
+        type: "string",
+      },
+      {
+        name: "caption",
+        label: "Caption",
+        type: "string",
+      },
+      {
+        name: "width",
+        label: "Width",
+        type: "number",
+      },
+      {
+        name: "height",
+        label: "Height",
+        type: "number",
+      },
+      {
+        name: "float",
+        label: "Float",
+        type: "string",
+        options: ["left", "right", "none"],
+      },
+      {
+        name: "shape",
+        label: "Shape",
+        type: "string",
+        options: ["square", "circle"],
+      },
+    ],
+  },
+];
+
 export default defineConfig({
   branch,
 
@@ -28,6 +91,35 @@ export default defineConfig({
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
+      {
+        name: "page",
+        label: "Pages",
+        path: "content/pages",
+        format: "mdx",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "description",
+            label: "Description",
+            list: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+            required: true,
+            templates,
+          },
+        ],
+      },
       {
         name: "post",
         label: "Posts",
